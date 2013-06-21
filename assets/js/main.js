c32b5d982a2972555b9701e95b49f8040ba83704
Jeremy Daley <daleyjem@yahoo.com>
2013-06-21 10:33:35 -0400

    custom config.xml file for build

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
+var clickAction;
+
 $(document).ready(function(){
-	$('#menuPopout').bind('touchstart', function(){
+	clickAction = Modernizr.touch ? 'touchstart' : 'click';
+	$('#menuPopout').bind(clickAction, function(){
 		$('#actionBar').toggleClass('open');
 	});
 });
