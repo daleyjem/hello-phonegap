00e847f94ed879c331e270d6effd10f54501b277
Jeremy Daley <daleyjem@yahoo.com>
2013-06-21 10:48:45 -0400

    scan implementation test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
+// Plugin includes
+var scanner = cordova.require('cordova/plugin/BarcodeScanner');
+
 var clickAction;
 
 $(document).ready(function(){
 	clickAction = Modernizr.touch ? 'touchstart' : 'click';
 	$('#menuPopout').bind(clickAction, function(){
 		$('#actionBar').toggleClass('open');
+	});
+	
+	$('#btn-scan').click(function(){
+		scanner.scan(function(result){
+			alert('scan success: ' + result.format);
+		}, function(){
+			alert('scan fail');
+		});
 	});
 });
