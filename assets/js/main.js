1719b531b677b923df62d60e0df8cbdf82dc4312
Jeremy Daley <daleyjem@yahoo.com>
2013-06-26 16:38:10 -0400

    removed alert()

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ var clickAction;
 
 document.addEventListener('deviceready', function(){
 	clickAction = 'touchstart'; //Modernizr.touch ? 'touchstart' : 'click';
-	alert(clickAction);
 	$('#menuPopout').bind(clickAction, function(){
 		$('#actionBar').toggleClass('open');
 	});
