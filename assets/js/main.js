c1a75952dc2b362ec7119f862c61f3d0b4333803
Jeremy Daley <daleyjem@yahoo.com>
2013-06-26 16:28:46 -0400

    'touchstart' hard-code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 var clickAction;
 
 document.addEventListener('deviceready', function(){
-	clickAction = Modernizr.touch ? 'touchstart' : 'click';
+	clickAction = 'touchstart'; //Modernizr.touch ? 'touchstart' : 'click';
 	alert(clickAction);
 	$('#menuPopout')[0].bind(clickAction, function(){
 		$('#actionBar').toggleClass('open');
