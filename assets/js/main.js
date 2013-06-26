b52c3a6a3ff7bdbfca24ca78832dba59a60c0e7e
Jeremy Daley <daleyjem@yahoo.com>
2013-06-26 16:34:46 -0400

    'touchstart' bind()... removed $()[0]

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@ var clickAction;
 document.addEventListener('deviceready', function(){
 	clickAction = 'touchstart'; //Modernizr.touch ? 'touchstart' : 'click';
 	alert(clickAction);
-	$('#menuPopout')[0].bind(clickAction, function(){
+	$('#menuPopout').bind(clickAction, function(){
 		$('#actionBar').toggleClass('open');
 	});
 });
